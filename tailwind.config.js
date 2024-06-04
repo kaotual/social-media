a0cc6c7ed8cfd4bbe42008c5bf89c68205a6a392
kao_tual <kaotualtd@gmail.com>
2024-06-04 21:29:16 +0700

    nghich

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
-      
+      margin: {
+        '1/2': '50%',
+        '1/4': '25%',
+      },
+      width: {
+        '1/2': '50%',
+        '1/4': '25%',
+      },
     },
   },
   plugins: [],
